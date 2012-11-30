dae63dfb99f80f8666da05ae7c5ab6fc7110fe93
hexfury <hexfury@gmail.com>
2012-11-30 14:20:45 -0400

    Lab7 completed

--- CS 1073/Lab7/CardHolder.java
+++ CS 1073/Lab7/CardHolder.java
@@ -1,13 +1,14 @@
 /**
 	@author Matthew Morgan, Scott Warren
 */
-
 public class CardHolder{
 	private String name;
 	private int aptNum;
 	private String phoneNum;
 	private static int memberNumIn = 1000;
 	private int memberNum;
+	protected int numberCheckedOut;
+	protected LendingItem[] signedOut;
 	
 	public CardHolder(String nameIn, int aptNumIn, String phoneNumIn){
 		name = nameIn;
@@ -15,34 +16,63 @@ public class CardHolder{
 		phoneNum = phoneNumIn;
 		memberNum = memberNumIn;
 		memberNumIn++;
-		signedOut = new LendingItem[7];
+		numberCheckedOut = 0;
 	}
-	
 	public String getName(){
 		return name;
 	}
-	
 	public int getAptNumber(){
 		return aptNum;
 	}
 	public void setPhoneNumber(String phoneNumIn){
 		phoneNum = phoneNumIn;
 	}
-	
 	public String getPhoneNumber(){
 		return phoneNum;
 	}
-	
-	public static int getMembershipNumber(){
+	public int getMembershipNumber(){
 		return memberNum;
 	}
-
 	public boolean signOut(LendingItem itemOut){
-		
+		if(numberCheckedOut == 0){
+			signedOut = new LendingItem[1];
+			signedOut[0] = itemOut;
+			numberCheckedOut++;
+			return true;
+		} else if (numberCheckedOut > 0 && numberCheckedOut < 7){
+			LendingItem[] temp = new LendingItem[numberCheckedOut+1];
+			for (int i = 0; i < numberCheckedOut; i++){
+				temp[i] = signedOut[i];
+			}
+			signedOut = temp;
+			signedOut[numberCheckedOut] = itemOut;
+			numberCheckedOut++;
+			return true;
+		}
+		return false;
 	}
-	
+	public boolean returnItem(LendingItem itemIn){
+		if(numberCheckedOut == 1){
+			signedOut[0] = null;
+			numberCheckedOut--;
+			return true;
+		} else {
+			for(int i = 0; i < signedOut.length; i++){
+				if(signedOut[i] == itemIn){
+					signedOut[i] = signedOut[numberCheckedOut-1];
+					signedOut[numberCheckedOut-1] = null;
+					numberCheckedOut--;
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 	public LendingItem[] getSignedOutItems(){
-		return signedOut;
+		LendingItem[] temp = new LendingItem[numberCheckedOut];
+		for(int i = 0; i < numberCheckedOut; i++){
+			temp[i] = signedOut[i];
+		}
+		return temp;
 	}
-	
 }
--- CS 1073/Lab7/LendingItem.java
+++ CS 1073/Lab7/LendingItem.java
@@ -24,6 +24,4 @@ public class LendingItem{
 	public boolean isRestricted(){
 		return restricted;
 	}
-	
-	
 }
